0603da09fe0c439815409fa80ba457d4d60191f7
Tur-nip <jinkhoo9286@gmail.com>
2024-11-12 15:45:19 +0800

    Delete src/main/java directory

--- src/main/java/Items.java
+++
@@ -1,7 +0,0 @@
-/**
- *
- * @author KhooWennHonn(TP065779)
- */
-public interface Items {
-    abstract void appendToFile();
-}
--- src/main/java/User.java
+++
@@ -1,7 +0,0 @@
-/**
- *
- * @author KhooWennHonn(TP065779)
- */
-public abstract class User {
-    abstract void appendFile();
-}
